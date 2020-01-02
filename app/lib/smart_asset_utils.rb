d72fa32c909e934fdfdd5b261b04c07fec87b37b
Igor Kasyanchuk <igorkasyanchuk@gmail.com>
2020-01-02 15:19:09 -0800

    fix;

--- app/lib/smart_asset_utils.rb
+++ app/lib/smart_asset_utils.rb
@@ -22,6 +22,7 @@ class SmartAssetUtils
     puts "create_tempfile_from: #{uri}"
     Net::HTTP.start(uri.host, uri.port, use_ssl: !!(url =~ /^https/)) do |http|
       resp = http.get(uri.path)
+      resp = http.get(resp.header['location']) if resp.code == "301"
       file = Tempfile.new(File.basename(url))
       file.binmode
       file.write(resp.body)
